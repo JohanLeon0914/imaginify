e9a97a4098fe4614dc3c6976f7abce23f226623c
Johan León <64605783+JohanLeon0914@users.noreply.github.com>
2024-02-20 11:02:55 -0500

    agregando grid en elementos de navegacion y quitando texto cuando se esta en mobile

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,27 +1,27 @@
-import { Collection } from "@/components/Shared/Collection"
-import { navLinks } from "@/constants"
-import { getAllImages } from "@/lib/actions/image.actions"
-import Image from "next/image"
-import Link from "next/link"
+import { Collection } from "@/components/Shared/Collection";
+import { navLinks } from "@/constants";
+import { getAllImages } from "@/lib/actions/image.actions";
+import Image from "next/image";
+import Link from "next/link";
 
 const Home = async ({ searchParams }: SearchParamProps) => {
   const page = Number(searchParams?.page) || 1;
-  const searchQuery = (searchParams?.query as string) || '';
+  const searchQuery = (searchParams?.query as string) || "";
 
-  const images = await getAllImages({ page, searchQuery})
+  const images = await getAllImages({ page, searchQuery });
 
   return (
     <>
       <section className="home">
-        <h1 className="home-heading">
+        <h1 className="home-heading hidden md:block">
           Unleash Your Creative Vision with Imaginify
         </h1>
-        <ul className="md:flex-center w-full gap-20 py-2">
+        <ul className="md:flex-center w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-10 md:gap-20">
           {navLinks.slice(1, 5).map((link) => (
             <Link
               key={link.route}
               href={link.route}
-              className="flex-center flex-col gap-2 mt-5"
+              className="flex-center flex-col gap-2"
             >
               <li className="flex-center w-fit rounded-full bg-white p-4">
                 <Image src={link.icon} alt="image" width={24} height={24} />
@@ -32,7 +32,7 @@ const Home = async ({ searchParams }: SearchParamProps) => {
         </ul>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
